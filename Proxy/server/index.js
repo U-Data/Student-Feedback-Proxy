653037c05ed6b7eb4d21b0c8b13aaccbbdb2338b
jimMartin4321 <jamesrm4321@gmail.com>
2018-11-25 18:08:29 -0800

    integrated stress testing

--- Proxy/server/index.js
+++ Proxy/server/index.js
@@ -1,12 +1,10 @@
+require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const proxy = require('express-http-proxy');
 
-require('../../Student-Feedback/server/index.js');
-require('../../instructors-service/server/index.js');
-require('../../header-sidebar-service/server/index.js');
-require('../../students-also-bought-service/server/server.js');
+// require('../Student-Feedback/server/index.js');
 
 const app = express();
 const PORT = 3000;
@@ -16,10 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/courses/:courseId', express.static(path.join(__dirname, '/../public')));
 
-app.get('/:courseId/reviews', proxy('http://localhost:3001'));
-app.get('/:id/instructors', proxy('http://localhost:3002'));
-app.get('/courses/:courseId/header', proxy('http://localhost:3003'));
-app.get('/courses/:courseId/similarcourses', proxy('http://localhost:3004'));
+app.get('/:courseId/reviews', proxy('http://localhost:3002'));
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
